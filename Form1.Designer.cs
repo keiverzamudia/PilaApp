5424eb4009534f09a8f991ed3eb87dc1bd6eb606
Keiver <keiberzamudia14@gmail.com>
2024-09-29 23:37:03 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace PilaApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.lstPila = new System.Windows.Forms.ListBox();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.btnPeek = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.BackColor = System.Drawing.SystemColors.Window;
+            this.txtElemento.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtElemento.Location = new System.Drawing.Point(142, 169);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(364, 20);
+            this.txtElemento.TabIndex = 0;
+            this.txtElemento.TextChanged += new System.EventHandler(this.txtElemento_TextChanged);
+            // 
+            // lstPila
+            // 
+            this.lstPila.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstPila.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstPila.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPila.FormattingEnabled = true;
+            this.lstPila.ItemHeight = 18;
+            this.lstPila.Location = new System.Drawing.Point(533, 17);
+            this.lstPila.Name = "lstPila";
+            this.lstPila.Size = new System.Drawing.Size(124, 146);
+            this.lstPila.TabIndex = 1;
+            this.lstPila.SelectedIndexChanged += new System.EventHandler(this.lstPila_SelectedIndexChanged);
+            // 
+            // btnPush
+            // 
+            this.btnPush.AutoSize = true;
+            this.btnPush.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnPush.Location = new System.Drawing.Point(40, 225);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(124, 51);
+            this.btnPush.TabIndex = 2;
+            this.btnPush.Text = "Push";
+            this.btnPush.UseVisualStyleBackColor = false;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // btnPop
+            // 
+            this.btnPop.AutoSize = true;
+            this.btnPop.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnPop.Location = new System.Drawing.Point(206, 225);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(124, 51);
+            this.btnPop.TabIndex = 3;
+            this.btnPop.Text = "Pop";
+            this.btnPop.UseVisualStyleBackColor = false;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // btnPeek
+            // 
+            this.btnPeek.AutoSize = true;
+            this.btnPeek.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPeek.Location = new System.Drawing.Point(370, 225);
+            this.btnPeek.Name = "btnPeek";
+            this.btnPeek.Size = new System.Drawing.Size(124, 51);
+            this.btnPeek.TabIndex = 4;
+            this.btnPeek.Text = "Peek";
+            this.btnPeek.UseVisualStyleBackColor = false;
+            this.btnPeek.Click += new System.EventHandler(this.btnPeek_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.AutoSize = true;
+            this.btnClear.BackColor = System.Drawing.Color.Red;
+            this.btnClear.Location = new System.Drawing.Point(533, 225);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(124, 51);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(157, 121);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(108, 25);
+            this.lblCount.TabIndex = 6;
+            this.lblCount.Text = "Contador";
+            this.lblCount.Click += new System.EventHandler(this.lblCount_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "C:\\Users\\Keiver Zamudia\\source\\repos\\PilaApp\\img\\1.png";
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(116, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(17, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 26);
+            this.button1.TabIndex = 8;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 311);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPeek);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.lstPila);
+            this.Controls.Add(this.txtElemento);
+            this.Controls.Add(this.pictureBox1);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.ListBox lstPila;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnPeek;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PilaApp
+{
+    public partial class Form1 : Form
+    {
+        Stack<string> pila;
+        public Form1()
+        {
+            InitializeComponent();
+            pila = new Stack<string>();
+            ActualizarListaPila();
+            this.CenterToScreen();
+        }
+        // Método para actualizar el ListBox y la etiqueta de cuenta
+        private void ActualizarListaPila()
+        {
+            lstPila.Items.Clear();
+            foreach (var item in pila)
+            {
+                lstPila.Items.Add(item);
+            }
+            lblCount.Text = $"Elementos en la pila: {pila.Count}";
+        }
+        private void txtElemento_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstPila_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Botón Push: Añadir un elemento a la pila
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtElemento.Text))
+            {
+                pila.Push(txtElemento.Text);
+                txtElemento.Clear();
+                ActualizarListaPila();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un elemento para añadir a la pila.");
+            }
+        }
+
+        // Botón Pop: Eliminar el elemento en el tope de la pila
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            if (pila.Count > 0)
+            {
+                string elemento = pila.Pop();
+                MessageBox.Show($"Elemento '{elemento}' eliminado de la pila.");
+                ActualizarListaPila();
+            }
+            else
+            {
+                MessageBox.Show("La pila está vacía.");
+            }
+        }
+
+        // Botón Peek: Ver el elemento en el tope sin eliminarlo
+        private void btnPeek_Click(object sender, EventArgs e)
+        {
+            if (pila.Count > 0)
+            {
+                string elemento = pila.Peek();
+                MessageBox.Show("Elemento en el tope: "+ elemento);
+            }
+            else
+            {
+                MessageBox.Show("La pila está vacía.");
+            }
+        }
+
+        // Botón Clear: Limpiar la pila
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pila.Clear();
+            MessageBox.Show("La pila ha sido vaciada.");
+            ActualizarListaPila();
+        }
+    
+
+    private void lblCount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = !pictureBox1.Visible;
+        }
+    }
+}
